2dad3be1eec5c5ef5c88d39bd6bef6f9993ce4e8
Meryem TAIKOK <meryem.taikok@oracle.com>
2023-06-05 14:06:59 +0100

    add email sender

---
+++ email_sender.py
@@ -0,0 +1,27 @@
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+email_sender = 'mery12tak@gmail.com'
+password = 'wefywkrypbogbbyi'
+
+email_receiver = 'meryemtaikok@gmail.com'
+
+subject = "Don't let me down"
+body = """
+Please don't forget to sat Hi to your mom!
+"""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context= context) as smtp:
+    smtp.login(email_sender, password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+
