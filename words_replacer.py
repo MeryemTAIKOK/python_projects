1e526fe65578eb8d066eb3f8f05bd311b8022f5e
Meryem TAIKOK <meryem.taikok@oracle.com>
2023-06-05 21:39:54 +0100

    words replacer

---
+++ words_replacer.py
@@ -0,0 +1,9 @@
+def words_replacer():
+
+    phrase="hi Mery, missing you hi hi hi hi. me too! hi hi hi"
+    word_to_replace = "hi"
+    word_replacer = "hey"
+    print(phrase.replace(word_to_replace, word_replacer))
+
+
+words_replacer()
