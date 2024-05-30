856a406fb5f0b791ac869dd5773868d23ef450d9
Meryem TAIKOK <meryem.taikok@oracle.com>
2024-05-30 11:02:17 +0100

    add other ways to print variables

--- crazy_python.py
+++ crazy_python.py
@@ -4,3 +4,11 @@ Verb_2 = input('Verb: ')
 famous_person = input('Famous person: ')
 
 print(f"Computer programming is so {Adjective} ! It makes me so excited all the time because I love to {Verb_1}. Stay hydrated and {Verb_2} like you are {famous_person}!")
+
+
+# a few ways to print String + variable in python
+#variable = "Something"
+
+#print("String and " + variable)
+#print("String and f)". format(variable))
+#print (f"String and {variable}")
