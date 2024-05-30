13727e02f48cc1eb327d1e261dd3edddab717f45
Meryem TAIKOK <meryem.taikok@oracle.com>
2024-05-30 10:45:57 +0100

    basic input/output

---
+++ crazy_python.py
@@ -0,0 +1,6 @@
+Adjective = input('Adjective: ')
+Verb_1 = input('Verb: ')
+Verb_2 = input('Verb: ')
+famous_person = input('Famous person: ')
+
+print(f"Computer programming is so {Adjective} ! It makes me so excited all the time because I love to {Verb_1}. Stay hydrated and {Verb_2} like you are {famous_person}!")
